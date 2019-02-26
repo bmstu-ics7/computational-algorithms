29110b81f210ab0c218485a56f454ad00df53af9
Willsem <happymainer132@yandex.ru>
2019-02-27 01:34:51 +0300

    ADD: second task

---
+++ lab_01/second.py
@@ -0,0 +1,24 @@
+from main import *
+
+def f(x):
+    return cos(x / 57.2958) - x
+
+
+def main():
+    table = create_table(f)
+    print_table(table)
+    print()
+
+    table[0], table[1] = table[1], table[0]
+    print_table(table)
+
+    n = int(input("Введите степень полинома: "))
+    print("x = 0")
+    x = 0
+
+    new_table = approximation(table, x, n)
+    print_approximation(new_table)
+    print(generate_polinom(new_table, x))
+
+if __name__ == "__main__":
+    main()
